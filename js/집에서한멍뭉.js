186c2d039c01221e8462d5a315f80199ea733464
kimhanbyeol-hash <wjsrlt12@gmail.com>
2024-10-29 17:46:05 +0900

    1월29일 멍
    10월 29일 화요일 하고있는 멍 프로젝트

--- js/집에서한멍뭉.js
+++ js/집에서한멍뭉.js
@@ -1,5 +1,5 @@
 // <!-- Initialize Swiper -->
-var swiper = new Swiper(".mySwiper", {
+var mainSwiper = new Swiper(".mainSwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
@@ -7,12 +7,38 @@ var swiper = new Swiper(".mySwiper", {
       disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
+      el: ".mainSwiper .swiper-pagination",
       clickable: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".mainSwiper .swiper-button-next",
+      prevEl: ".mainSwiper .swiper-button-prev",
     },
   });
   
+
+
+  //  Initialize Swiper
+        var pushSwiper = new Swiper(".pushSwiper", {
+            slidesPerView: 1.3, //처음 보여지는 슬라이드 갯수, 초기값은 모바일
+            spaceBetween: 30,   //슬라이드 마진
+            pagination: {
+                el: ".pushSwiper .swiper-pagination",
+                clickable: true,
+            },
+            breakpoints: {
+            768: {
+                slidesPerView: 2.2,  //브라우저가 768보다 클 때
+                spaceBetween: 40,
+            },
+            1024: {
+                slidesPerView: 
+                4.9,  //브라우저가 1024보다 클 때
+                spaceBetween: 20,
+            },
+            },
+            navigation: {
+        nextEl: ".pushSwiper .swiper-button-next",
+        prevEl: ".pushSwiper .swiper-button-prev",
+      },
+        });
